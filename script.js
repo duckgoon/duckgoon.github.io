4eab3dcd8f64ba2c3c97ce9d5aa5020b6d271982
duckgoon <muhiburrahim051@gmail.com>
2025-09-12 01:43:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // Sample data (could come from an API)
 const siteData = {
   title: "My JavaScript Website",
-  nav: ["Projects", "Programming Blog", "BA English Portfolio",  "Pictures worth a thousand words", "Terms", "Privacy"],
+  nav: ["Projects", "Programming Blog", "BA English Portfolio",  "Pictures worth a thousand words", "Research Articles", "Terms", "Privacy"],
   content: {
     Projects: "Here are some of our projects.",
     Programming_Blog: "My daily dose of software lectures",
     BA_English_Portfolio: "Writing everyday - poems, prose, play, journal, arts.",
     Pictures: "Photos taken by me",
+    Research_Articles: "Condoning daily research habit.",
     Terms: "No terms, you could send me a message if you intend to copy anything.",
     Privacy: "For educational purposes, this site uses cookies."
   }
